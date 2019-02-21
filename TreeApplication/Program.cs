f9a7d56a49b1a34276f2de869675d332b1fb3cef
simonkasp <simonas.kasparas@gmail.com>
2019-02-21 22:34:47 +0200

    Initial commit

---
+++ TreeApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TreeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saka medis =
+                new Saka(1,
+                    new Saka(2,
+                        new Saka(3)),
+                    new Saka(4,
+                        new Saka(5,
+                            new Saka(6)),
+                        new Saka(7,
+                            new Saka(8,
+                                new Saka(9)),
+                            new Saka(10)),
+                        new Saka(11))
+           );
+
+            medis.GiliausiasLygis();
+        }
+    }
+}
---
+++ TreeApplication/Saka.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeApplication
+{
+    public class Saka
+    {
+        private List<Saka> sakos = new List<Saka>();
+        private readonly int sk;
+        private Saka pagrindineSaka;
+
+        public int VaikuSkaicius
+        {
+            get { return sakos.Count(); }
+        }
+
+        public Saka(int sk)
+        {
+            this.sk = sk;
+        }
+
+        public Saka(int sk, params Saka[] sakos)
+            : this(sk)
+        {
+            
+            pagrindineSaka = new Saka(sk);
+
+            foreach (Saka vaikas in sakos)
+            {
+                pagrindineSaka.PridetiVaika(vaikas.pagrindineSaka);
+            }
+        }
+
+        public void PridetiVaika(Saka vaikas)
+        {
+            sakos.Add(vaikas);
+        }
+
+        public Saka GautiVaika(int index)
+        {
+            return sakos[index];
+        }
+
+        public void GiliausiasLygis()
+        {
+            Console.Write("Giliausias lygis: ");
+            Console.WriteLine(GiliausiasLygis(pagrindineSaka, 1));
+        }
+
+        private int GiliausiasLygis(Saka pagrindineSaka, int gylis)
+        {
+            Saka vaikas;
+
+            if (pagrindineSaka == null)
+                return 0;
+            
+            int rezultatas = gylis + 1;
+
+            for (int i = 0; i < pagrindineSaka.VaikuSkaicius; i++)
+            {
+                vaikas = pagrindineSaka.GautiVaika(i);
+                rezultatas = Math.Max(rezultatas, GiliausiasLygis(vaikas, gylis + 1));
+            }
+
+            return rezultatas;
+        }
+
+    }
+}
